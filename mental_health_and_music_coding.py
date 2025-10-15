efc03f62817fba3ac03ab8e0db3b5347693d1bee
hiepc <hiepcarole@gmail.com>
2025-10-15 12:03:05 -0400

    Dataset
    Dataset for our lia

---
+++ mental_health_and_music_coding.py
@@ -0,0 +1,40 @@
+#DATASET
+import pandas as pd
+
+data = pd.read_csv("mxmh_survey_results.csv")
+
+#Age
+Age = data["Age"]
+print(Age)
+
+#Primary Streaming Service
+Primary_Streaming_Service = data["Primary streaming service"]
+print(Primary_Streaming_Service)
+
+#Hours Per Day
+Hours_Per_Day = data["Hours per day"]
+print(Hours_Per_Day)
+
+#While Working
+While_Working = data['While working']
+print(While_Working)
+
+#Instrumentalist
+Instrumentalist = data["Instrumentalist"]
+print(Instrumentalist)
+
+#Composer
+Composer = data["Composer"]
+print(Composer)
+
+#Fav genre
+Fav_genre = data["Fav genre"]
+print(Fav_genre)
+
+#Exploratory
+Exploratory = data["Exploratory"]
+print(Exploratory)
+
+#Foreign languages
+Foreign_languages = data["Foreign languages"]
+print(Foreign_languages)
