2ca2189b3ff0e1f7960c7af2957006f39a851f96
hiepc <hiepcarole@gmail.com>
2025-10-15 21:00:26 -0400

    Update dataset Carole
    dataset

--- mental_health_and_music_coding.py
+++ mental_health_and_music_coding.py
@@ -38,3 +38,115 @@ print(Exploratory)
 #Foreign languages
 Foreign_languages = data["Foreign languages"]
 print(Foreign_languages)
+
+#BPM
+BPM = data["BPM"]
+print(BPM)
+
+#Frequency[Classical]
+Frequency_Classical = data["Frequency [Classical]"]
+print(Frequency_Classical)
+
+#Frequency[Country]
+Frequency_Country = data["Frequency [Country]"]
+print(Frequency_Country)
+
+#Frequency[EDM]
+Frequency_EDM = data["Frequency [EDM]"]
+print(Frequency_EDM)
+
+#Frequency[Folk]
+Frequency_Folk = data["Frequency [Folk]"]
+print(Frequency_Folk)
+
+#Frequency[Gospel]
+Frequency_Gospel = data["Frequency [Gospel]"]
+print(Frequency_Gospel)
+
+#Frequency[Hip hop]
+Frequency_Hip_hop = data["Frequency [Hip hop]"]
+print(Frequency_Hip_hop)
+
+#Frequency[Jazz]
+Frequency_Jazz = data["Frequency [Jazz]"]
+print(Frequency_Jazz)
+
+#Frequency[K pop]
+Frequency_K_pop = data["Frequency [K pop]"]
+print(Frequency_K_pop)
+
+#Frequency[Latin]
+Frequency_Latin = data["Frequency [Latin]"]
+print(Frequency_Latin)
+
+#Frequency[Lofi]
+Frequency_Lofi = data["Frequency [Lofi]"]
+print(Frequency_Lofi)
+
+#Frequency[Metal]
+Frequency_Metal = data["Frequency [Metal]"]
+print(Frequency_Metal)
+
+#Frequency[Pop]
+Frequency_Pop = data["Frequency [Pop]"]
+print(Frequency_Pop)
+
+#Frequency[R&B]
+Frequency_RNB = data["Frequency [R&B]"]
+print(Frequency_RNB)
+
+#Frequency[Rap]
+Frequency_Rap = data["Frequency [Rap]"]
+print(Frequency_Rap)
+
+#Frequency[Rock]
+Frequency_Rock = data["Frequency [Rock]"]
+print(Frequency_Rock)
+
+#Frequency[Video game music]
+Frequency_VGM = data["Frequency [Video game music]"]
+print(Frequency_VGM)
+
+#Anxiety
+anxiety = data["Anxiety"]
+print(anxiety)
+
+#Depression
+depression = data["Depression"]
+print(depression)
+
+#Insomnia
+insomnia = data["Insomnia"]
+print(insomnia)
+
+#OCD
+OCD = data["OCD"]
+print(OCD)
+
+#Music effects
+music_effects = data["Music effects"]
+print(music_effects)
+
+#LOOPS AND CONDITIONALS
+
+for BPM in BPM  :
+    if BPM > 0:
+        print(BPM)
+        
+for music_effects in music_effects:
+    if music_effects == "Improve":
+        print(music_effects)
+    elif music_effects == "Worsen":
+        print(music_effects)
+    
+
+#PLOTS
+import matplotlib.pyplot as plt
+
+plt.title("Hours of music per day by age group")
+plt.xlabel("Age")
+plt.ylabel("Hours Per Day")
+y = Hours_Per_Day
+x = Age
+plt.bar(x, y)
+plt.show()
