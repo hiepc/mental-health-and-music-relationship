46eb4dc129a4a23ded6b187010bfb57abd591341
alicerancu <alice.i.rancu@gmail.com>
2025-11-12 23:48:38 -0500

    Update mental_health_and_music_coding.py

--- mental_health_and_music_coding.py
+++ mental_health_and_music_coding.py
@@ -191,6 +191,7 @@ data = pd.read_csv('mxmh_survey_results.csv')
 
 print(data.describe())
 
+<<<<<<< Updated upstream
 
 
 sns.relplot(data=data, y="Hours per day", x="Age", hue="Music effects")
@@ -242,3 +243,8 @@ g = sns.relplot(data=data, y="Hours per day", x="Age", hue="Music effects")
 g.fig.suptitle("Title: Hours per day listened to music based on the age group and the benefits from it", fontsize=12, fontweight='bold')
 g.fig.subplots_adjust(top=0.85)
 
+=======
+g = sns.relplot(data=data, y="Hours per day", x="Age", hue="Music effects")
+g.fig.suptitle("Title: Hours per day listened to music based on the age group and the benefits from it", fontsize=12, fontweight='bold')
+g.fig.subplots_adjust(top=0.85)
+>>>>>>> Stashed changes
